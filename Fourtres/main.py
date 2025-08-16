34264a8aa3eab64d6a0b9e3ec5ec15beef429cce
kjcabPL <kcabrera@profoundlogic.com>
2025-08-16 15:08:20 +0800

    Initial Commit

---
+++ Fourtres/main.py
@@ -0,0 +1,252 @@
+import tkinter
+from json import JSONDecodeError
+from tkinter.ttk import Combobox
+
+import pyperclip
+import json
+from tkinter import *
+from tkinter import messagebox
+from random import randint, choice, shuffle
+
+IS_OPEN = True
+GEN_PW = ""
+GEN_MAX_CTR = 100
+GEN_CALC = False
+# STOREFILE = "data.txt"
+STOREFILE = "stores.json"
+STORED_DATA = {}
+CUR_USER_DATA = []
+PW_LENGTH = 20
+
+# Variables for Color Manipulation during PW generation
+CUR_BG = ""
+canvasbgStart = "#FF0000"
+canvasbgGen = "#FFF088"
+startColor = (255, 0, 0)
+endColor = (255, 240, 136)
+incs = ( int((endColor[0] - startColor[0]) / GEN_MAX_CTR), int((endColor[1] - startColor[1]) / GEN_MAX_CTR), int((endColor[2] - startColor[2]) / GEN_MAX_CTR))
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def genPassword():
+    global GEN_PW
+    global GEN_CALC
+    global CUR_BG
+
+    GEN_CALC = True
+    GEN_CUR_CTR = 0
+    CUR_BG = startColor
+    doGenAnimation(GEN_CUR_CTR)
+    tbNewPw.insert(0, str(GEN_PW))
+    disableButtons()
+    canvas.config(bg=canvasbgStart)
+
+def doGenAnimation(CTR):
+    global GEN_PW
+    global GEN_CALC
+    global CUR_BG
+
+    if GEN_CALC:
+        if CTR < GEN_MAX_CTR:
+            CTR += 1
+            GEN_PW = CTR
+            newPass = genRandomizedHash()
+            tbNewPw.delete(0, tkinter.END)
+            tbNewPw.insert(0, newPass)
+            CUR_BG = ( CUR_BG[0] + incs[0], CUR_BG[1] + incs[1], CUR_BG[2] + incs[2] )
+            bgColor = f"#{CUR_BG[0]:02X}{CUR_BG[1]:02X}{CUR_BG[2]:02X}"
+            canvas.config(bg=bgColor)
+            window.after(10, doGenAnimation, CTR)
+        else:
+            GEN_CALC = False
+            pyperclip.copy(tbNewPw.get())
+            disableButtons(False)
+            canvas.config(bg=canvasbgGen)
+
+def genRandomizedHash():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    # Separate the password length to letter, number and symbol variance
+    maxChars = PW_LENGTH
+    nr_letters = randint(int(maxChars / 4), int(maxChars / 2))
+    maxChars -= nr_letters
+    nr_numbers = randint(int(maxChars / 3), int(maxChars / 2))
+    maxChars -= nr_numbers
+    nr_symbols = maxChars
+
+    # Generate a new password from combined list comprehension arithmetic
+    newPass = [choice(letters) for _ in range(0, nr_letters)] + [choice(numbers) for _ in range(0, nr_numbers)] + [choice(symbols) for _ in range(0, nr_symbols)]
+    shuffle(newPass)
+    newPass = "".join(newPass)
+    return newPass
+
+
+# ---------------------------- PASSWORD FUNCTIONS ------------------------------- #
+def writeNewStoreFile():
+    with open(STOREFILE, mode="w") as file:
+        file.write("")
+
+def savePWData():
+    newpw = tbNewPw.get()
+    website = tbWebsite.get()
+    user = tbUser.get()
+
+    if website == "":
+        messagebox.showerror("Error Saving Data", "Website should not be empty")
+        return
+    elif user == "":
+        messagebox.showerror("Error Saving Data", "Username should not be empty")
+        return
+
+    # convert to json data
+    curData = ""
+    data = {
+        website: [{
+            user: {
+                "pw": newpw
+            }
+        }]
+    }
+
+    # open the contents of the store file
+    try:
+        with open(STOREFILE, mode="r") as file:
+            curData = json.load(file)
+    except FileNotFoundError:
+            writeNewStoreFile()
+            curData = data
+    except JSONDecodeError:
+        curData = data
+    else:
+        curData.update(data)
+
+    # TODO - if a website record already exists, ADD the user/email and its pw to that list instead
+    # TODO - if a website already HAS that user/email, PROMPT before replacing the record
+    # record updated contents
+    try:
+        with open(STOREFILE, mode="w") as file:
+             json.dump(curData, file, indent=2)
+    except FileNotFoundError:
+        messagebox.showerror("Error Saving Data", "Unable to find store file: " + STOREFILE)
+    else:
+        messagebox.showinfo("New Record Added", f"Password entry for {website} added to store file.")
+    finally:
+        tbNewPw.delete(0,tkinter.END)
+        tbWebsite.delete(0, tkinter.END)
+        tbUser.delete(0, tkinter.END)
+
+def readPWData():
+    global STORED_DATA
+    try:
+        with open(STOREFILE, mode="r") as file:
+            STORED_DATA = json.load(file)
+    except FileNotFoundError:
+        writeNewStoreFile()
+    except JSONDecodeError:
+        STORED_DATA = {}
+
+def searchPWData():
+    global CUR_USER_DATA
+    userlist = []
+    website = tbWebsite.get()
+    if website == "":
+        messagebox.showerror("Search Error", "No Website Provided")
+        return
+    try:
+        user_data = [ data for data in STORED_DATA[website] if website in STORED_DATA ]
+    except KeyError:
+        messagebox.showerror("Search Error", f"No Users recorded for website: {website}")
+        CUR_USER_DATA = []
+    else:
+        if len(user_data) > 0:
+            # for items in user_data:
+            for user in user_data[0]:
+                username = user
+                userlist.append(username)
+                pw = user_data[0][user]["pw"]
+
+                tbUser.delete(0,tkinter.END)
+                tbUser.insert(0, username)
+                tbNewPw.delete(0,tkinter.END)
+                tbNewPw.insert(0, pw)
+                CUR_USER_DATA.append({ "user": user, "pw": pw })
+
+    finally:
+        listUsers["values"] = tuple(userlist)
+        listUsers.set("")
+        if len(userlist) > 0:
+            listUsers.set(userlist[0])
+
+# ---------------------------- UI SETUP ------------------------------- #
+def closeWindow():
+    global IS_OPEN
+    if messagebox.askokcancel("Exit", "Exit Fourtres?"):
+        IS_OPEN = False
+        window.destroy()
+
+def disableButtons(state = True):
+    val = tkinter.DISABLED
+    if not state:
+        val = tkinter.NORMAL
+
+    btGenPW["state"] = val
+    btAdd["state"] = val
+    btSearch["state"] = val
+
+window = Tk()
+window.config(padx=25, pady=25)
+window.title("Fourtres Password Builder")
+window.resizable(False, False)
+readPWData()
+canvasbg = window["bg"]
+
+lock_img = PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200)
+canvas.config(bg=canvasbg)
+canvas.create_image(100, 100, image=lock_img )
+
+# labels
+lblWebsite = Label(text="Website:", width = 15 )
+lblUser = Label(text="Email/Username:", width = 15)
+lblNewPW = Label(text="New Password:", width = 15)
+
+# Entry Fields
+tbWebsite = Entry(width = 57)
+tbWebsite.focus()
+tbUser = Entry(width = 57)
+tbUser.insert(0,"dummy@gmail.com")
+tbNewPw = Entry(width = 33)
+
+# buttons & other comps
+btGenPW = Button(text = "Generate Password", width = 19)
+btAdd = Button(text = "Add Record", width = 48)
+btSearch = Button(text = "Search Website Records", width = 28)
+
+listUsers = Combobox(values=[])
+listUsers.set("")
+
+# function assignments
+window.protocol("WM_DELETE_WINDOW", closeWindow)
+btGenPW["command"] = genPassword
+btAdd["command"] = savePWData
+btSearch["command"] = searchPWData
+
+# arrangements
+canvas.grid(row = 1, column = 1)
+lblWebsite.grid(row = 2, column = 0)
+tbWebsite.grid(row = 2, column = 1, columnspan = 2)
+lblUser.grid(row = 3, column = 0)
+tbUser.grid(row = 3, column = 1, columnspan = 2)
+lblNewPW.grid(row = 4, column = 0)
+tbNewPw.grid(row = 4, column = 1)
+btGenPW.grid(row = 4, column = 2)
+btAdd.grid(row = 5, column = 1, columnspan = 2)
+btSearch.grid(row = 6, column = 1)
+listUsers.grid(row = 6, column = 2)
+
+# window loop
+while IS_OPEN:
+    window.mainloop()
